457e4725443dcf428c0bd1c679d63458474f3755
vaalenin <vaalenin@gmail.com>
2020-04-07 20:40:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,48 @@
+#define NO_VIRTUAL 
 
+#include <vector>
+
+#ifdef NO_VIRTUAL
+#include "shape_no_virtual.h"
+#else
+#include "shape_virtual.h"
+#endif
+
+std::vector <Shape*> shapes;
+
+void CreateShapes (std::vector <Shape*>& s)
+{
+  for (int i = 0; i < 100; i ++)
+  {
+    int shapeType = rand () % 3;
+    
+    switch (shapeType)
+    {
+      case 0:
+        s.push_back(new Triangle);
+        break;
+      case 1:
+        s.push_back(new Quad);
+        break;
+      case 2:
+        s.push_back(new Circle);
+        break;
+    }
+  }
+}
+
+void PrintShapes (const std::vector <Shape*>& s)
+{
+  for (const auto& shape: s)
+    s.Draw ();
+}
+
+int main ()
+{
+  srand (time(nullptr));
+  
+  CreateShapes (shapes);
+  PrintShapes (shapes);
+  
+  return 0;
+}
