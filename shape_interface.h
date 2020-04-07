591f09d4e56bb001938bcdc21c48a3b31740d70d
vaalenin <vaalenin@gmail.com>
2020-04-07 21:08:32 +0300

    Update shape_interface.h

--- shape_interface.h
+++ shape_interface.h
@@ -1,9 +1,15 @@
+#pragma once
+
 #include <iostream>
 
+//виртуальный базовый класс
 class Shape
 {
   protected:
     int mainColor = 0;
     int linesColor = 0;
-    virtual void Draw () = 0;
+    virtual void Draw () = 0; //чистая виртуальная функция
 };
+
+//в современном C++, а также в C#, Java и других ООП-языках такие классы называют "Интерфейс"
+//такие классы часто используются для реализации плагинов
